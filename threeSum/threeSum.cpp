0deece40741580e790a996914bec313554af2ecb
Your Name <zhao.henry14@gmail.com>
2026-09-06 00:41:46 -0500

    threesum!

---
+++ threeSum/threeSum.cpp
@@ -0,0 +1,93 @@
+#include "threeSum/threeSum.h"
+
+#include <type_traits>
+
+int main() {
+    // Triple testing
+    using R1 = three_sum<
+        9,
+        util::value_list<int, 1, 2, 3, 4, 5, 6, 7>
+    >::result;
+    static_assert(R1::size == 3, "Failure");
+    static_assert(
+        R1::contains<util::value_list<int, 1, 2, 6>>,
+        "Failure"
+    );
+    static_assert(
+        R1::contains<util::value_list<int, 1, 3, 5>>,
+        "Failure"
+    );
+    static_assert(
+        R1::contains<util::value_list<int, 2, 3, 4>>,
+        "Failure"
+    );
+
+    using R2 = three_sum<
+        10,
+        util::value_list<int, 1, 2, 3, 5, 7, 8>
+    >::result;
+    static_assert(R2::size == 2, "Failure");
+    static_assert(
+        R2::contains<util::value_list<int, 1, 2, 7>>,
+        "Failure"
+    );
+    static_assert(
+        R2::contains<util::value_list<int, 2, 3, 5>>,
+        "Failure"
+    );
+
+    using R3 = three_sum<
+        10,
+        util::value_list<int, 5, 5, 7, 8, -3, -2>
+    >::result;
+    static_assert(R3::size == 2, "Failure");
+    static_assert(
+        R3::contains<util::value_list<int, 5, 8, -3>>,
+        "Failure"
+    );
+    static_assert(
+        R3::contains<util::value_list<int, 5, 7, -2>>,
+        "Failure"
+    );
+
+    using R4 = three_sum<
+        0,
+        util::value_list<int, -5, -2, 0, 2, 5, 7>
+    >::result;
+    static_assert(R4::size == 3, "Failure");
+    static_assert(
+        R4::contains<util::value_list<int, -5, -2, 7>>,
+        "Failure"
+    );
+    static_assert(
+        R4::contains<util::value_list<int, -5, 0, 5>>,
+        "Failure"
+    );
+    static_assert(
+        R4::contains<util::value_list<int, -2, 0, 2>>,
+        "Failure"
+    );
+
+    // No solution
+    using R5 = three_sum<
+        100,
+        util::value_list<int, 1, 2, 3, 4, 5>
+    >::result;
+    static_assert(R5::empty, "Failure");
+
+    // Fewer than three elements
+    using R6 = three_sum<
+        5,
+        util::value_list<int, 5, 1>
+    >::result;
+    static_assert(R6::empty, "Failure");
+
+    // Empty list
+    using R7 = three_sum<
+        5,
+        util::value_list<int>
+    >::result;
+    static_assert(R7::empty, "Failure");
+
+    return 0;
+}
---
+++ threeSum/threeSum.h
@@ -0,0 +1,68 @@
+#ifndef threeSum_threeSum_h
+#define threeSum_threeSum_h
+
+#include <type_traits>
+#include "util/type_list.h"
+#include "util/value_list.h"
+
+// three_sum inner
+template <int Val, int A, typename List, typename Acc>
+struct three_sum_inner;
+
+template <int Val, int A, typename Acc>
+struct three_sum_inner<Val, A, util::value_list<int>, Acc>
+{
+    using result = Acc;
+};
+
+template <int Val, int A, int B, int... Tail, typename Acc>
+struct three_sum_inner<Val, A, util::value_list<int, B, Tail...>, Acc>
+{
+private:
+    static constexpr int C = Val - A - B;
+    static constexpr bool Exists =
+        util::value_list<int, Tail...>::template contains<C>;
+    
+    using Triple = util::value_list<int, A, B, C>;
+    using New_Acc = typename std::conditional<
+        Exists && !Acc::template contains<Triple>,
+        typename Acc::template append<Triple>,
+        Acc
+    >::type;
+
+public:
+    using result = typename three_sum_inner<
+        Val,
+        A,
+        util::value_list<int, Tail...>,
+        New_Acc
+    >::result;
+};
+
+
+// three_sum
+template <int Val, typename List, typename Acc = util::type_list<>>
+struct three_sum;
+
+template <int Val, typename Acc>
+struct three_sum<Val, util::value_list<int>, Acc>
+{
+    using result = Acc;
+};
+
+template <int Val, int A, int... Tail, typename Acc>
+struct three_sum<Val, util::value_list<int, A, Tail...>, Acc>
+{
+    using result = typename three_sum<
+        Val,
+        util::value_list<int, Tail...>,
+        typename three_sum_inner<
+            Val,
+            A,
+            util::value_list<int, Tail...>,
+            Acc
+        >::result
+    >::result;
+};
+
+#endif
