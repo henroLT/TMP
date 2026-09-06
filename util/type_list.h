178a43f0d20753e2e07e4f8c4f3d742045f5ec88
Your Name <zhao.henry14@gmail.com>
2026-09-06 00:39:41 -0500

    namespace formatting and at<> support

--- util/type_list.h
+++ util/type_list.h
@@ -1,8 +1,27 @@
-#ifndef util_type_list
-#define util_type_list
+#ifndef util_type_list_h
+#define util_type_list_h
 
 #include <type_traits>
 #include <cstddef>
+
+/*
+    * value_list
+    *
+    * Operations:
+    *   size:            Number of elements.
+    *   empty:           Whether the list is empty.
+    *   contains<Val>:   Whether Val is in the list.
+    *   append<Val>:     Appends Val to the end.
+    *   remove<Val>:     Removes the first occurrence of Val.
+    *   remove_all<Val>: Removes all occurrences of Val.
+    *   at<Idx>:         Retrieves the value at index Idx.
+    *                    at<Idx> is only valid for valid indices.
+    *
+    * For value_list<>:
+    *   typeify<T>:     Converts an unspecified value_list to value_list<T>.
+*/
+
+namespace util {
 
 template <typename... Ts>
 struct type_list;
@@ -11,62 +30,78 @@ namespace detail {
 
 // Contains
 template <typename T, typename List>
-struct contains;
+struct type_list_contains;
 
 template <typename T>
-struct contains<T, type_list<>> : 
+struct type_list_contains<T, type_list<>> : 
     std::false_type {};
 
 template <typename T, typename Head, typename... Tail>
-struct contains<T, type_list<Head, Tail...>> :
+struct type_list_contains<T, type_list<Head, Tail...>> :
     std::conditional<
         std::is_same<T, Head>::value,
         std::true_type,
-        contains<T, type_list<Tail...>>
+        type_list_contains<T, type_list<Tail...>>
     >::type
 {};
 
 
 // Remove First Occurance
 template <typename T, typename List, typename Acc = type_list<>>
-struct remove;
+struct type_list_remove;
 
 template <typename T, typename... Acc>
-struct remove<T, type_list<>, type_list<Acc...>>
+struct type_list_remove<T, type_list<>, type_list<Acc...>>
 {
     using type = type_list<Acc...>;
 };
 
 template <typename T, typename... Tail, typename... Acc>
-struct remove<T, type_list<T, Tail...>, type_list<Acc...>>
+struct type_list_remove<T, type_list<T, Tail...>, type_list<Acc...>>
 {
     using type = type_list<Acc..., Tail...>;
 };
 
 template <typename T, typename Head, typename... Tail, typename... Acc>
-struct remove<T, type_list<Head, Tail...>, type_list<Acc...>> :
-    remove<T, type_list<Tail...>, type_list<Acc..., Head>>
+struct type_list_remove<T, type_list<Head, Tail...>, type_list<Acc...>> :
+    type_list_remove<T, type_list<Tail...>, type_list<Acc..., Head>>
 {};
 
 
 // Remove All Occurance
 template <typename T, typename List, typename Acc = type_list<>>
-struct remove_all;
+struct type_list_remove_all;
 
 template <typename T, typename... Acc>
-struct remove_all<T, type_list<>, type_list<Acc...>>
+struct type_list_remove_all<T, type_list<>, type_list<Acc...>>
 {
     using type = type_list<Acc...>;
 };
 
 template <typename T, typename... Tail, typename... Acc>
-struct remove_all<T, type_list<T, Tail...>, type_list<Acc...>> :
-    remove_all<T, type_list<Tail...>, type_list<Acc...>>
+struct type_list_remove_all<T, type_list<T, Tail...>, type_list<Acc...>> :
+    type_list_remove_all<T, type_list<Tail...>, type_list<Acc...>>
 {};
 
 template <typename T, typename Head, typename... Tail, typename... Acc>
-struct remove_all<T, type_list<Head, Tail...>, type_list<Acc...>> :
-    remove_all<T, type_list<Tail...>, type_list<Acc..., Head>>
+struct type_list_remove_all<T, type_list<Head, Tail...>, type_list<Acc...>> :
+    type_list_remove_all<T, type_list<Tail...>, type_list<Acc..., Head>>
+{};
+
+
+// At
+template <std::size_t Target, typename List>
+struct type_list_at;
+
+template <typename Head, typename... Tail>
+struct type_list_at<0, type_list<Head, Tail...>>
+{
+    using type = Head;
+};
+
+template <std::size_t Target, typename Head, typename... Tail>
+struct type_list_at<Target, type_list<Head, Tail...>> :
+    type_list_at<Target - 1, type_list<Tail...>>
 {};
 
 } // namespace detail
@@ -83,7 +118,7 @@ struct type_list
     static constexpr bool empty = false;
 
     template <typename Elem>
-    static constexpr bool contains = detail::contains<
+    static constexpr bool contains = detail::type_list_contains<
         Elem, type_list<Ts...>
     >::value;
 
@@ -91,13 +126,18 @@ struct type_list
     using append = type_list<Ts..., Elem>;
 
     template <typename Elem>
-    using remove = detail::remove<
+    using remove = detail::type_list_remove<
         Elem, type_list<Ts...>
     >::type;
 
     template <typename Elem>
-    using remove_all = detail::remove_all<
+    using remove_all = detail::type_list_remove_all<
         Elem, type_list<Ts...>
+    >::type;
+
+    template <std::size_t Idx>
+    using at = detail::type_list_at<
+        Idx, type_list<Ts...>
     >::type;
 };
 
@@ -120,5 +160,7 @@ struct type_list<>
     template <typename Elem>
     using remove_all = type_list<>;
 };
+
+} // namespace util
 
 #endif
--- util/value_list.h
+++ util/value_list.h
@@ -1,8 +1,28 @@
-#ifndef util_value_list
-#define util_value_list
+#ifndef util_value_list_h
+#define util_value_list_h
 
 #include <type_traits>
 #include <cstddef>
+
+/*
+    * value_list
+    *
+    * Operations:
+    *   size:             Number of elements.
+    *   empty:            Whether the list is empty.
+    *   contains<Val>:    Whether Val occurs in the list.
+    *   append<Val>:      Appends Val to the end.
+    *   remove<Val>:      Removes the first occurrence of Val.
+    *   remove_all<Val>:  Removes all occurrences of Val.
+    *   at<Idx>:          Retrieves the value at index Idx.
+    *                     Only valid for valid indexs
+    *
+    * value_list<> represents an untyped list
+    *   must use typeify<T> to create a typed value_list.
+    *
+*/
+
+namespace util {
 
 template <typename T = void, T... Ts>
 struct value_list;
@@ -11,62 +31,78 @@ namespace detail {
 
 // Contains
 template <typename T, T Elem, typename List>
-struct contains;
+struct value_list_contains;
 
 template <typename T, T Elem>
-struct contains<T, Elem, value_list<T>> :
+struct value_list_contains<T, Elem, value_list<T>> :
     std::false_type {};
 
 template <typename T, T Elem, T Head, T... Tail>
-struct contains<T, Elem, value_list<T, Head, Tail...>> :
+struct value_list_contains<T, Elem, value_list<T, Head, Tail...>> :
     std::conditional<
         Elem == Head,
         std::true_type,
-        contains<T, Elem, value_list<T, Tail...>>
+        value_list_contains<T, Elem, value_list<T, Tail...>>
     >::type
 {};
 
 
 // Remove
 template <typename T, T Elem, typename List, typename Acc = value_list<T>>
-struct remove;
+struct value_list_remove;
 
 template <typename T, T Elem, T... Acc>
-struct remove<T, Elem, value_list<T>, value_list<T, Acc...>>
+struct value_list_remove<T, Elem, value_list<T>, value_list<T, Acc...>>
 {
     using type = value_list<T, Acc...>;
 };
 
 template <typename T, T Elem, T... Tail, T... Acc>
-struct remove<T, Elem, value_list<T, Elem, Tail...>, value_list<T, Acc...>>
+struct value_list_remove<T, Elem, value_list<T, Elem, Tail...>, value_list<T, Acc...>>
 {
     using type = value_list<T, Acc..., Tail...>;
 };
 
 template <typename T, T Elem, T Head, T... Tail, T... Acc>
-struct remove<T, Elem, value_list<T, Head, Tail...>, value_list<T, Acc...>> :
-    remove<T, Elem, value_list<T, Tail...>, value_list<T, Acc..., Head>>
+struct value_list_remove<T, Elem, value_list<T, Head, Tail...>, value_list<T, Acc...>> :
+    value_list_remove<T, Elem, value_list<T, Tail...>, value_list<T, Acc..., Head>>
 {};
 
 
 // Remove All
 template <typename T, T Elem, typename List, typename Acc = value_list<T>>
-struct remove_all;
+struct value_list_remove_all;
 
 template <typename T, T Elem, T... Acc>
-struct remove_all<T, Elem, value_list<T>, value_list<T, Acc...>>
+struct value_list_remove_all<T, Elem, value_list<T>, value_list<T, Acc...>>
 {
     using type = value_list<T, Acc...>;
 };
 
 template <typename T, T Elem, T... Tail, T... Acc>
-struct remove_all<T, Elem, value_list<T, Elem, Tail...>, value_list<T, Acc...>> :
-    remove_all<T, Elem, value_list<T, Tail...>, value_list<T, Acc...>>
+struct value_list_remove_all<T, Elem, value_list<T, Elem, Tail...>, value_list<T, Acc...>> :
+    value_list_remove_all<T, Elem, value_list<T, Tail...>, value_list<T, Acc...>>
 {};
 
 template <typename T, T Elem, T Head, T... Tail, T... Acc>
-struct remove_all<T, Elem, value_list<T, Head, Tail...>, value_list<T, Acc...>> :
-    remove_all<T, Elem, value_list<T, Tail...>, value_list<T, Acc..., Head>>
+struct value_list_remove_all<T, Elem, value_list<T, Head, Tail...>, value_list<T, Acc...>> :
+    value_list_remove_all<T, Elem, value_list<T, Tail...>, value_list<T, Acc..., Head>>
+{};
+
+
+// At
+template <std::size_t Target, typename List>
+struct value_list_at;
+
+template <typename T, T Head, T... Tail>
+struct value_list_at<0, value_list<T, Head, Tail...>>
+{
+    static constexpr T value = Head;
+};
+
+template <std::size_t Target, typename T, T Head, T... Tail>
+struct value_list_at<Target, value_list<T, Head, Tail...>> :
+    value_list_at<Target - 1, value_list<T, Tail...>>
 {};
 
 } // namespace detail
@@ -83,18 +119,27 @@ struct value_list
     static constexpr bool empty = false;
 
     template <T Elem>
-    static constexpr bool contains = detail::contains<
+    using append = value_list<T, Ts..., Elem>;
+
+    template <T Elem>
+    static constexpr bool contains = detail::value_list_contains<
         T, Elem, value_list<T, Ts...>
     >::value;
 
     template <T Elem>
-    using remove = detail::remove<T, Elem, value_list<T, Ts...>>::type;
+    using remove = detail::value_list_remove<
+        T, Elem, value_list<T, Ts...>
+    >::type;
 
     template <T Elem>
-    using remove_all = detail::remove_all<T, Elem, value_list<T, Ts...>>::type;
+    using remove_all = detail::value_list_remove_all<
+        T, Elem, value_list<T, Ts...>
+    >::type;
 
-    template <T Elem>
-    using append = value_list<T, Ts..., Elem>;
+    template <std::size_t Idx>
+    static constexpr T at = detail::value_list_at<
+        Idx, value_list<T, Ts...>
+    >::value;
 };
 
 // Empty Value List
@@ -105,6 +150,9 @@ struct value_list<T>
     static constexpr bool empty = true;
 
     template <T Elem>
+    using append = value_list<T, Elem>;
+
+    template <T Elem>
     static constexpr bool contains = false;
 
     template <T Elem>
@@ -112,9 +160,6 @@ struct value_list<T>
 
     template <T Elem>
     using remove_all = value_list<T>;
-
-    template <T Elem>
-    using append = value_list<T, Elem>;
 };
 
 // Unspecified Value List
@@ -125,5 +170,7 @@ struct value_list<>
     template <typename T>
     using typeify = value_list<T>;
 };
+
+} // namespace util
 
 #endif
