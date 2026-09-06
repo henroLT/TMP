a8a3d866932f3495b96fe7ca3c417a060b829ad8
Your Name <zhao.henry14@gmail.com>
2026-09-06 00:40:00 -0500

    comply with new util

--- twoSum/twoSum.cpp
+++ twoSum/twoSum.cpp
@@ -4,31 +4,58 @@
 
 int main() {
     // Pair testing
-    using R1 = twosum<9, value_list<int, 1, 2, 3, 4, 5, 6, 7>>::result;
-    static_assert(R1::first + R1::second == 9, "");
-    std::cout << R1::first << " " << R1::second << '\n';
+    using R1 = twosum<
+        9,
+        util::value_list<int, 1, 2, 3, 4, 5, 6, 7>
+    >::result;
+    static_assert(R1::size == 2, "Failure");
+    static_assert(R1::at<0> + R1::at<1> == 9, "Failure");
+    std::cout << R1::at<0> << " " << R1::at<1> << '\n';
 
-    using R2 = twosum<20, value_list<int, 1, 3, 5, 7, 9, 11, 13>>::result;
-    static_assert(R2::first + R2::second == 20, "");
-    std::cout << R2::first << " " << R2::second << '\n';
+    using R2 = twosum<
+        20,
+        util::value_list<int, 1, 3, 5, 7, 9, 11, 13>
+    >::result;
+    static_assert(R2::size == 2, "Failure");
+    static_assert(R2::at<0> + R2::at<1> == 20, "Failure");
+    std::cout << R2::at<0> << " " << R2::at<1> << '\n';
 
-    using R3 = twosum<10, value_list<int, 5, 5, 7, 8>>::result;
-    static_assert(R3::first + R3::second == 10, "");
-    std::cout << R3::first << " " << R3::second << '\n';
+    using R3 = twosum<
+        10,
+        util::value_list<int, 5, 5, 7, 8>
+    >::result;
+    static_assert(R3::size == 2, "Failure");
+    static_assert(R3::at<0> + R3::at<1> == 10, "Failure");
+    std::cout << R3::at<0> << " " << R3::at<1> << '\n';
 
-    using R4 = twosum<0, value_list<int, -5, -2, 3, 5, 10>>::result;
-    static_assert(R4::first + R4::second == 0, "");
-    std::cout << R4::first << " " << R4::second << '\n';
+    using R4 = twosum<
+        0,
+        util::value_list<int, -5, -2, 3, 5, 10>
+    >::result;
+    static_assert(R4::size == 2, "Failure");
+    static_assert(R4::at<0> + R4::at<1> == 0, "Failure");
+    std::cout << R4::at<0> << " " << R4::at<1> << '\n';
 
     // No solution
-    using R5 = twosum<100, value_list<int, 1, 2, 3, 4, 5>>::result;
-    static_assert(std::is_same<R5, NoResult>::value, "");
+    using R5 = twosum<
+        100,
+        util::value_list<int, 1, 2, 3, 4, 5>
+    >::result;
+    static_assert(R5::empty, "Failure");
 
     // Single element cannot form a pair
-    using R6 = twosum<5, value_list<int, 5>>::result;
-    static_assert(std::is_same<R6, NoResult>::value, "");
+    using R6 = twosum<
+        5,
+        util::value_list<int, 5>
+    >::result;
+    static_assert(R6::empty, "Failure");
 
     // Empty list
-    using R7 = twosum<5, value_list<int>>::result;
-    static_assert(std::is_same<R7, NoResult>::value, "");
+    using R7 = twosum<
+        5,
+        util::value_list<int>
+    >::result;
+    static_assert(R7::empty, "Failure");
+
+    return 0;
 }
--- twoSum/twoSum.h
+++ twoSum/twoSum.h
@@ -1,43 +1,29 @@
-#ifndef twoSum_twoSum
-#define twoSum_twoSum
+#ifndef twoSum_twoSum_h
+#define twoSum_twoSum_h
 
 #include <type_traits>
 #include "util/value_list.h"
-
-// result
-template <int A, int B>
-struct Result {
-   static constexpr int first = A;
-   static constexpr int second = B;
-};
-struct NoResult{};
 
 // Two Sum
 template <int Val, typename List>
 struct twosum;
 
 template <int Val>
-struct twosum<Val, value_list<int>>
+struct twosum <Val, util::value_list<int>>
 {
-   static constexpr bool found = false;
-   using result = NoResult;
+   using result = util::value_list<int>;
 };
 
 template <int Sum, int Head, int... Tail>
-struct twosum<Sum, value_list<int, Head, Tail...>>
+struct twosum <Sum, util::value_list<int, Head, Tail...>>
 {
-private:
-   static constexpr bool here =
-      value_list<int, Tail...>::template contains<Sum - Head>;
-
-public:
-   static constexpr bool found =
-      here || twosum<Sum, value_list<int, Tail...>>::found;
-   
    using result = typename std::conditional<
-      here,
-      Result<Head, Sum - Head>,
-      typename twosum<Sum, value_list<int, Tail...>>::result
+      util::value_list<int, Tail...>::template contains<Sum - Head>,
+      util::value_list<int, Head, Sum - Head>,
+      typename twosum<
+         Sum,
+         util::value_list<int, Tail...>
+      >::result
    >::type;
 };
 
